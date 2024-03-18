3ee460ce187af0be8eb8e91001cc8c1833514ba5
simbacargo <info@simbacargo.com>
2024-03-18 15:05:35 +0300

    fixed connector

--- config/settings/test.py
+++ config/settings/test.py
@@ -192,7 +192,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'normal_user_austh',
+        'NAME': 'normal_user_auth',
         'USER': 'root',
         'PASSWORD': '',
         'HOST':'localhost',
